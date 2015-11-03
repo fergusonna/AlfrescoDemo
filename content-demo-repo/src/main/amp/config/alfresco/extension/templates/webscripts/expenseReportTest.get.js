fc0da35899116b1943c5c2124950d54ca6bad2b3
Nick Ferguson <nickferguson.naf@gmail.com>
2015-11-03 11:53:44 -0500

    Changed expenseReportTest to randomize various metadata properties

--- content-demo-repo/src/main/amp/config/alfresco/extension/templates/webscripts/expenseReportTest.get.js
+++ content-demo-repo/src/main/amp/config/alfresco/extension/templates/webscripts/expenseReportTest.get.js
@@ -3,15 +3,43 @@ var documentName = url.templateArgs.documentName;
 var currentTimeMillis = new Date().getTime();
 documentName += "_" + currentTimeMillis.toString();
 
-var randNum = Math.random()*10;
+var randNum = Math.random()*100; //used to randomize set properties
+
+//Folder for my place reports into
+var folder = companyhome.childByNamePath("_ExpenseReports");
+
+//If folder doesn't exist, then it is created
+if (folder == undefined){
+	folder = companyhome.createFolder("_ExpenseReports");
+}
 
 //Array to keep the properties in.
 var properties = new Array();
+
 properties["my:totalAmount"] = randNum.toFixed(2);
-properties["my:product"] = "Flux Capacitor"; 
+properties["my:clientName"] = "MyCo";
+
+//Randomizing the Related Product
+randNum = Math.floor(Math.random()*3);
+if(randNum == 1){
+	properties["my:product"] = "Flux Capacitor"; 
+} else if (randNum == 2){
+	properties["my:product"] = "Foobar Widget";
+} else {
+	properties["my:product"] = "Big Bold App";
+}
+//Random Expense Type
+randNum = Math.floor(Math.random()*3);
+if(randNum == 1){
+	properties["my:expenseType"] = "Hours Billable"; 
+} else if (randNum == 2){
+	properties["my:expenseType"] = "Supply Expense";
+} else {
+	properties["my:expenseType"] = "License Cost";
+}
 
 //Create document
-var document = companyhome.createNode(documentName, contentType, properties);
+var document = folder.createNode(documentName, contentType, properties);
 
 if (document != null){
 	model.document = document;
